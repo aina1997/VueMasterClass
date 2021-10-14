e774a3cb47072c12a46d7bc3ddb4d11fcf963254
aina1997 <ainapalacioscorral@gmail.com>
2021-10-14 14:40:49 +0200

    masterclass

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-md-4">
+    <div class="card m50 p50">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text"></p>
+        <a href="#" class="btn btn-primary" >Dale</a>
+      </div>
+      <button></button>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'ButtonCustom',
+  props:[
+    
+  ],
+  data() {
+    return{
+      colors: ['red', 'pink', 'blue', 'cyan', 'green', 'yellow'],
+      
+    }
+  }
+}
+</script>
+
